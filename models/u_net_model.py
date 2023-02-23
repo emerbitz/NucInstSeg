54abe3d2ceb497c9ee634cdeb60f1075a4d3189e
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-02-23 08:20:43 +0000

    U-Net model for initial experiments

---
+++ models/u_net_model.py
@@ -0,0 +1,35 @@
+from typing import NoReturn
+import pytorch_lightning as pl
+import torch
+
+
+class UNetModel(pl.LightningModule):
+    def __int__(self, model) -> NoReturn:
+        self.model = model
+        self.lr = 2e-4
+        self.loss = torch.nn.BCELoss()
+
+    def forward(self, x) -> torch.Tensor:
+        return self.model(x)
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        y_hat = self.forward(x)
+        loss = self.loss(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        y_hat = self.forward(x)
+        loss = self.loss(y_hat, y)
+        self.log("val_loss", loss)
+
+    def test_step(self, test_batch, batch_idx):
+        x, y = test_batch
+        y_hat = self.forward(x)
+        loss = self.loss(y_hat, y)
+        self.log("test_loss", loss)
