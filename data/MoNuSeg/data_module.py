0ee51f5b90b33e6a09cd2d457a7e80453d3a10e1
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-02-24 11:30:28 +0100

    Added data preprocessing and augmentation Enabled multi-process data loading Added data_root argument

--- data/MoNuSeg/data_module.py
+++ data/MoNuSeg/data_module.py
@@ -1,9 +1,12 @@
 from typing import NoReturn
 import pytorch_lightning as pl
+import torch
 from torch.utils.data import DataLoader, random_split
 
 from data.MoNuSeg.dataset import MoNuSeg
 from data.MoNuSeg.dataset_creator import MoNuSegCreator
+from augmentation.augmentations import RandCrop, RandHorizontalFlip, RandRotate, RandVerticalFlip
+from transformation.transformations import Combine, ToTensor
 
 
 class MoNuSegDataModule(pl.LightningDataModule):
@@ -11,15 +14,30 @@ class MoNuSegDataModule(pl.LightningDataModule):
     Performs splitting and batching into train, validation and test for the MoNuSeg dataset
     """
 
-    def __init__(self, seg_masks: bool = True, cont_masks: bool = True, dist_maps: bool = True):
+    def __init__(self, seg_masks: bool = True, cont_masks: bool = True, dist_maps: bool = True,
+                 data_root: str = "datasets"):
         super().__init__()
         self.seg_masks = seg_masks
         self.cont_masks = cont_masks
         self.dist_maps = dist_maps
-        self.batch_size = 4
+        self.root = data_root
+        self.batch_size = 12
+        self.threads = torch.get_num_threads()
+        self.train_transforms = Combine([
+            ToTensor(),
+            RandHorizontalFlip(p=0.5),
+            RandVerticalFlip(p=0.5),
+            RandRotate(degrees=360.),
+            RandCrop(size=(256, 256))
+        ])
+        self.val_transforms = Combine([  # Currently no effect
+            ToTensor(),
+            RandCrop(size=(256, 256))
+        ])
+        self.test_transforms = None  # To do
 
     def prepare_data(self) -> NoReturn:
-        creator = MoNuSegCreator(root="datasets")
+        creator = MoNuSegCreator(root=self.root)
         creator.save_ground_truths(
             segmentation_masks=self.seg_masks,
             contour_masks=self.cont_masks,
@@ -29,28 +47,31 @@ class MoNuSegDataModule(pl.LightningDataModule):
     def setup(self, stage: str = None) -> NoReturn:
         if stage == "fit" or stage is None:
             data = MoNuSeg(
-                root="datasets",
+                root=self.root,
                 segmentation_mask=self.seg_masks,
                 contour_mask=self.cont_masks,
                 distance_map=self.dist_maps,
-                split="Train"
+                split="Train",
+                transforms=self.train_transforms
             )
-            self.train_data, self.val_data = random_split(data, lengths=[12, 4])
+            # Problem?: Same Transformation for train and val split
+            self.train_data, self.val_data = random_split(data, lengths=[12,4])
 
         if stage == "test" or stage is None:
             self.test_data = MoNuSeg(
-                root="datasets",
+                root=self.root,
                 segmentation_mask=self.seg_masks,
                 contour_mask=self.cont_masks,
                 distance_map=self.dist_maps,
-                split="Test"
+                split="Test",
+                transforms=self.test_transforms
             )
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=False, num_workers=self.threads)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=False, num_workers=self.threads)
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.test_data, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self.test_data, batch_size=self.batch_size, shuffle=False, num_workers=self.threads)
