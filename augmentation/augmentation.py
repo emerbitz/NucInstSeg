1e38075407693fbe8942902a8697b54b97a98df5
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-02-24 09:37:39 +0100

    Moved class Combine to transformation.py

--- augmentation/augmentation.py
+++ augmentation/augmentation.py
@@ -1,23 +1,13 @@
 import random
-from typing import List, Tuple
+from typing import Tuple
 import torch
 import torchvision.transforms.functional as TF
 
-from augmentation_base import Augmentation
-
-
-class Combine:
-    def __init__(self, augmentations: List[Augmentation]):
-        self.augmentations = augmentations
-
-    def __call__(self, imgs: List[torch.Tensor]) -> List[torch.Tensor]:
-        for augmentation in self.augmentations:
-            imgs = augmentation(imgs)
-        return imgs
+from augmentation.augmentation_base import Augmentation
 
 
 class RandHorizontalFlip(Augmentation):
-    def __init__(self, p: float=0.5):
+    def __init__(self, p: float = 0.5):
         self.p = p
 
     def transform(self, img: torch.Tensor, seed: float) -> torch.Tensor:
@@ -42,6 +32,7 @@ class RandRotate(Augmentation):
     """
     Rotates the image by an angle.
     """
+
     def __init__(self, degrees: float = 360.):
         self.deg = degrees
 
@@ -55,6 +46,7 @@ class RandCrop(Augmentation):
     """
     Generates a random crop from the image of size (h, w).
     """
+
     def __init__(self, size: Tuple[int, int]):
         self.size = size
 
@@ -68,44 +60,25 @@ class RandCrop(Augmentation):
         w_lower = random.randint(0, w_max)
         w_upper = w_lower + self.size[1]
         random.randint(0, w_max)
-        return img[:,h_lower:h_upper, w_lower:w_upper]
+        return img[:, h_lower:h_upper, w_lower:w_upper]
 
 
 if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    import torchvision.transforms as T
     from data.MoNuSeg.dataset import MoNuSeg
     from data.MoNuSeg.illustrator import Picture
+    from transformation.transformation import ToTensor, Combine
 
-    # img = MoNuSeg(root="../datasets")[0][0]
-    # flipped = F.hflip(img)
-    # rotated = F.rotate(img, angle=360)
-    #
-    # Picture.from_tensor(img).show()
-    # Picture.from_tensor(flipped).show()
-    # Picture.from_tensor(rotated).show()
+    transforms = Combine([
+        ToTensor(),
+        RandHorizontalFlip(p=0.5),
+        RandCrop(size=(10, 10))
+    ])
 
-    Picture.from_tensor(MoNuSeg(root="../datasets")[0][0]).show()
-    for img in RandCrop(size=(255, 255))(MoNuSeg(root="../datasets")[0]):
-        pic = Picture.from_tensor(img)
+    train_data = MoNuSeg(root="../datasets", split="Train", transforms=transforms)
+    for pair in train_data:
+        print(type(pair[0]))
+        pic = Picture.from_tensor(pair[0])
         print(pic.size())
         pic.show()
-
-    # augments = Combine([
-    #     RandVerticalFlip(p=1),
-    #     RandHorizontalFlip(p=1),
-    #     RandRotate(),
-    #     RandCrop(size=(700, 700))
-    # ])
-    # for img in augments(MoNuSeg(root="../datasets")[0]):
-    #     pic = Picture.from_tensor(img)
-    #     pic.show()
-
-    # from torch.utils.data import DataLoader
-    # import torchvision.transforms as T
-    # train_dataset = MoNuSeg(root="../datasets", split="Train")
-    # train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-    # batch = next(iter(train_dataloader))
-    # imgs, seg_masks, cont_masks, dist_maps = batch
-    # # # imgs, [seg_masks, cont_masks, dist_maps] = batch
-    # # flipped = T.RandomCrop(size=255)(batch)
-    # print(len(batch))
-    #
