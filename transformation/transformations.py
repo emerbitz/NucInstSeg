ca2c6379be5f013d96e853fc7cc323ba94e4ea57
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-03-15 14:52:37 +0100

    Improved class Split

--- transformation/transformations.py
+++ transformation/transformations.py
@@ -7,7 +7,7 @@ import torchvision.transforms.functional as f
 
 from augmentation.augmentation_base import Augmentation
 from transformation.transformation_base import Transformation
-
+from transformation.utils import remove_zero_stacks, ceiling
 
 class Combine:
     """Allows for the combination of Augmentation and Transformation subclasses"""
@@ -24,8 +24,8 @@ class Combine:
 class Resize(Transformation):
     """Resizes the image to a given size"""
 
-    def __init__(self, size: List[int], interpolation: f.InterpolationMode = f.InterpolationMode.BILINEAR) -> NoReturn:
-        self.size = size
+    def __init__(self, size: Tuple[int, int], interpolation: f.InterpolationMode = f.InterpolationMode.BILINEAR) -> NoReturn:
+        self.size = list(size)
         self.interpolation = interpolation
 
     def transform(self, img: torch.Tensor) -> torch.Tensor:
@@ -48,33 +48,24 @@ class Split(Transformation):
 
     def transform(self, img: torch.Tensor) -> List[torch.Tensor]:
         _, img_height, img_width = img.size()
-        num_splits_y, num_splits_x = -(img_height // -self.size[0]), -(img_width // -self.size[1])  # Ceiling division
-        padding = (num_splits_y * self.size[0] - img_height, num_splits_x * self.size[1] - img_width)
+        size_y, size_x = self.size
+        num_splits_y, num_splits_x = ceiling(img_height, size_y), ceiling(img_width, size_x)
+        padding = (ceiling(num_splits_y * size_y - img_height, 2), ceiling(num_splits_x * size_x - img_width, 2))
         if padding != (0, 0):
             img = self.pad_zeros(img=img, padding=padding)
 
         output = []
-        for y in range(0, img_height, self.size[0]):
-            for x in range(0, img_width, self.size[1]):
-                output.append(img[:, y: y + self.size[0], x: x + self.size[1]])
+        for y in range(0, img_height, size_y):
+            for x in range(0, img_width, size_x):
+                patch = img[:, y: y + size_y, x: x + size_x]
+                patch = remove_zero_stacks(patch)
+                output.append(patch)
         return output
 
     @staticmethod
-    def pad_zeros(img: torch.Tensor, padding: Union[Tuple[int, int], int]):
-        """Pads the image on the height and width axis with the given number of zeros."""
-        if not isinstance(padding, (tuple, int)):
-            raise TypeError(f"Padding should be of type tuple or int. Got instead '{type(padding)}'.")
-        if isinstance(padding, tuple) and len(padding) != 2:
-            raise ValueError(
-                f"Padding as tuple should contain a value for height and width each. Got instead {len(padding)} values.")
-        if isinstance(padding, int):
-            padding = [0, 0, padding, padding]
-            # padding = (padding, padding)
-        else:
-            padding = [0, 0, padding[0], padding[1]]
-            # padding = (padding[0], padding[1])
-
-        return f.pad(img=img, padding=padding, fill=0.)
+    def pad_zeros(img: torch.Tensor, padding: Union[Tuple[int, int], int]) -> torch.Tensor:
+        """Pads the image on all sides with the given number of zeros."""
+        return PadZeros(padding=padding).transform(img=img)
 
 
 class PadZeros(Transformation):
