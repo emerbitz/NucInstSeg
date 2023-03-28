a9d405b729a6dc81c636595b7f8ea1d90de50cc6
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-03-28 12:40:10 +0200

    Added logging of evaluation metrics

--- models/al_net_model.py
+++ models/al_net_model.py
@@ -1,11 +1,14 @@
-from typing import NoReturn
 import pytorch_lightning as pl
 import torch
+from torch import Tensor
 import torch.nn as nn
+
+from postprocessing.segmentation import NucleiSplitter
+from evaluation.metrics import PQ, AJI
 
 
 class ALNetModel(pl.LightningModule):
-    def __init__(self, model) -> NoReturn:
+    def __init__(self, model) -> None:
         super(ALNetModel, self).__init__()
         self.model = model
         self.lr = 2e-4
@@ -13,7 +16,10 @@ class ALNetModel(pl.LightningModule):
         self.loss_main = nn.BCEWithLogitsLoss()  # Penalty term to be added
         self.loss_aux = nn.BCEWithLogitsLoss()
 
-    def forward(self, x) -> torch.Tensor:
+        self.pq_val, self.pq_test = PQ(), PQ()
+        self.aji_val, self.aji_test = AJI(), AJI()
+
+    def forward(self, x) -> Tensor:
         return self.model(x)
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
@@ -27,10 +33,37 @@ class ALNetModel(pl.LightningModule):
         return loss
 
     def validation_step(self, val_batch, batch_idx):
-        imgs, seg_masks, cont_masks = val_batch
+        imgs, seg_masks, cont_masks, instances = val_batch
         seg, cont = self.forward(imgs)
         loss = self.lambda_main * self.loss_main(seg, seg_masks) + self.lambda_aux * self.loss_aux(cont, cont_masks)
         self.log("val_loss", loss)
 
+        pred_instances = NucleiSplitter(seg=seg, cont=cont).to_instances()
+        self.pq_val.update(pred_instances, instances)
+        self.aji_val.update(pred_instances, instances)
+
+    def validation_epoch_end(self, outputs):
+        dq, sq, pq = self.pq_val.compute()
+        self.log("val_dq", dq)
+        self.log("val_sq", sq)
+        self.log("val_pq", pq)
+        self.log("val_aji", self.aji_val.compute())
+        self.pq_val.reset(), self.aji_val.reset()
+
     def test_step(self, test_batch, batch_idx):
-        pass
+        imgs, seg_masks, cont_masks, instances = test_batch
+        seg, cont = self.forward(imgs)
+        loss = self.lambda_main * self.loss_main(seg, seg_masks) + self.lambda_aux * self.loss_aux(cont, cont_masks)
+        self.log("test_loss", loss)
+
+        pred_instances = NucleiSplitter(seg=seg, cont=cont).to_instances()
+        self.pq_test.update(pred_instances, instances)
+        self.aji_test.update(pred_instances, instances)
+
+    def test_epoch_end(self, outputs):
+        dq, sq, pq = self.pq_test.compute()
+        self.log("test_dq", dq)
+        self.log("test_sq", sq)
+        self.log("test_pq", pq)
+        self.log("test_aji", self.aji_test.compute())
+        self.pq_test.reset(), self.aji_test.reset()
