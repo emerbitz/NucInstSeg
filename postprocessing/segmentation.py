20b344ee99c2e18467d8c9acc5a3be036b13cb85
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-04-14 08:46:07 +0000

    Rewrote class NucleiSplitter

--- postprocessing/segmentation.py
+++ postprocessing/segmentation.py
@@ -3,38 +3,55 @@ import torch
 from torch import Tensor
 
 from data.MoNuSeg.ground_truth import NucleiInstances
+from postprocessing.region_growing import RegionGrower
 from evaluation.utils import is_batched
 
-class NucleiSplitter:
-    """Splits clustered nuclei through removal of the nuclei contours into nuclei instances.
-    Parameters:
-        seg: Segmentation as tensor with shape (B, C, H, W)
-        cont: Contours as tensor with shape (B, C, H, W)
-    """
-    def __init__(self, seg: Tensor, cont: Tensor):
-        seg_mask = seg > 0.5  # Thresholding operation to obtain the segmentation masks e [0, 1]^(B, C, H, W)
-        cont_mask = cont > 0.5 # Thresholding operation to obtain the contour masks e [0, 1]^(B, C, H, W)
-        self._instance_mask = self.split(seg_mask, cont_mask)
 
+class InstanceExtractor:
+    """Postprocesses the segmentation and extracts instances."""
+
+    def __init__(self, seg: Tensor, cont: Tensor = None) -> None:
+        """
+        :param seg: Segmentation as tensor with shape (B, C, H, W)
+        :param cont: Contours as tensor with shape (B, C, H, W)
+        """
+        seg_mask = seg > 0.5  # Thresholding operation to obtain the segmentation masks e {0, 1}^(B, C, H, W)
         if not is_batched(seg):
-            raise ValueError("Seg should have shape (B, C, H, W).")
-        if not is_batched(cont):
-            raise ValueError("Cont should have shape (B, C, H, W).")
+            raise ValueError(f"Seg should have shape (B, C, H, W). Got instead {seg.shape}")
+
+        if cont is not None:
+            cont_mask = cont > 0.5  # Thresholding operation to obtain the contour masks e [0, 1]^(B, C, H, W)
+            if not is_batched(cont):
+                raise ValueError(f"Cont should have shape (B, C, H, W). Got instead {cont.shape}")
+            # Removes the contours from the segmentation in order to split instances
+            self.mask = torch.logical_and(seg_mask, torch.logical_not(cont_mask))
+        else:
+            self.mask = seg_mask
+
+        self.cuda = seg.is_cuda
         self.batch_size = seg.shape[0]
 
-    @staticmethod
-    def split(seg_mask: Tensor, cont_mask: Tensor) -> Tensor:
-        """Removes the contours from the segmented nuclei."""
-        return torch.logical_and(seg_mask, torch.logical_not(cont_mask))
+    def get_instances(self, impl: str = "skimage") -> Union[Tensor, Tuple[Tensor, ...]]:
+        """Extracts the instance.
+        :param
+        impl: Implementation of the region growing algorithm for instance extraction. Can be either custom or skimage.
+        """
+        if impl == "skimage":
+            if self.batch_size == 1:
+                if self.cuda:
+                    return NucleiInstances.from_seg_mask(self.mask.cpu()).as_tensor().cuda()
+                else:
+                    return NucleiInstances.from_seg_mask(self.mask).as_tensor()
+            else:
+                if self.cuda:
+                    return tuple(NucleiInstances.from_seg_mask(mask.cpu()).as_tensor().cuda() for mask in self.mask)
+                else:
+                    return tuple(NucleiInstances.from_seg_mask(mask).as_tensor() for mask in self.mask)
 
-    @property
-    def instance_mask(self):
-        return self._instance_mask
-
-    def to_instances(self) -> Union[Tensor, Tuple[Tensor, ...]]:
-        """Extracts the nuclei instances from the instance mask."""
-        if self.batch_size == 1:
-            return NucleiInstances.from_seg_mask(self._instance_mask).as_tensor()
+        elif impl == "custom":
+            if self.batch_size == 1:
+                return RegionGrower(self.mask).get_regions()
+            else:
+                return tuple(RegionGrower(mask).get_regions() for mask in self.mask)
         else:
-            return tuple(NucleiInstances.from_seg_mask(mask).as_tensor() for mask in self._instance_mask)
-
+            raise ValueError(f"Impl should be custom or skimage. Got instead {impl}.")
