b3ea4f8ab06284bacdc5cd5d727ecbc75f1c7fac
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-04-26 09:40:37 +0200

    Can now create overlays

--- data/MoNuSeg/illustrator.py
+++ data/MoNuSeg/illustrator.py
@@ -3,10 +3,12 @@ from torch import Tensor
 from typing import Tuple
 from pathlib import Path
 import matplotlib.pyplot as plt
+from skimage.color import label2rgb
 
+from data.MoNuSeg.ground_truth import NucleiInstances
 
 class Picture:
-    """Bundles type conversion and depiction of images"""
+    """Bundles type conversion and depiction of images."""
 
     def __init__(self, img: np.array):
         if not isinstance(img, np.ndarray):
@@ -14,18 +16,29 @@ class Picture:
         self.data = img
 
     def size(self) -> Tuple[int]:
-        """Gives the shape of the image"""
+        """Gives the shape of the Picture."""
         return self.data.shape
 
     @staticmethod
-    def from_tensor(img: Tensor) -> "Picture":
-        """Converts an image as tensor into an instance of the Picture class"""
-        img = Picture.tensor_to_ndarray(img)
+    def from_tensor(img: Tensor, inst: Tensor = None) -> "Picture":
+        """Creates the Picture from the tensor image (img). If the tensor nuclei instances (inst) is provided as well,
+        the Picture is created from the overlay of the image with the nuclei instances."""
+        if inst is not None:
+            img = Picture.overlay(img, inst)  # inst: shape (#nuclei, H, W)
+        else:
+            img = Picture.tensor_to_ndarray(img)
         return Picture(img)
 
     @staticmethod
+    def overlay(img: Tensor, inst: Tensor) -> np.ndarray:
+        """Overlays the tensor image (img) with the tensor nuclei instances (inst)."""
+        img = Picture.tensor_to_ndarray(img)
+        labeled_inst = NucleiInstances.from_inst(inst).to_labeled_inst()  # Maybe more direct implementation would be better
+        return label2rgb(label=labeled_inst, image=img)
+
+    @staticmethod
     def tensor_to_ndarray(img: Tensor) -> np.array:
-        """Converts tensor into np.ndarray"""
+        """Converts a tensor into np.ndarray."""
         dim = img.dim()
         if dim == 3:
             channels, *_ = img.size()
@@ -41,13 +54,13 @@ class Picture:
             raise ValueError(f"Img should have two or three dimensions. Got instead {img.dim()} dimensions.")
 
     def show(self) -> None:
-        """Displays the image"""
+        """Displays the Picture."""
         plt.imshow(self.data, cmap="gray")  # Cmap argument is ignored for RGB data per default
         plt.axis("off")
         plt.show()
 
     def save(self, file_name: str, save_dir: str = "images"):
-        """Saves the image as png"""
+        """Saves the Picture as png."""
         save_dir = Path(save_dir)
         file = Path(save_dir, file_name + ".png")
         if not save_dir.is_dir():
