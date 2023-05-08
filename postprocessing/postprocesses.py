3ef69e199433de5ea0b234bd066f732eee599e05
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-05-08 09:53:52 +0200

    Postprocessing classes

---
+++ postprocessing/postprocesses.py
@@ -0,0 +1,92 @@
+from typing import Optional, Tuple, Union
+from torch import Tensor
+
+from postprocessing.postprocesses_base import Postprocess
+
+
+class SegPostProcess(Postprocess):
+    """
+    Postprocessing based on the segmentation mask and the contour mask.
+
+    Nuclei instances are extracted by flooding. If a contour mask is provided, then the contour mask is subtracted from
+    the segmentation mask prior to flooding.
+    """
+    def __init__(self):
+        pass
+
+    def __call__(self, seg: Tensor, cont: Optional[Tensor] = None) -> Union[Tensor, Tuple[Tensor, ...]]:
+        seg_mask = seg >= 0.5
+        if cont is not None:
+            cont_mask = cont >= 0.5
+        else:
+            cont_mask = cont
+        return self.postprocess(seg_mask, cont_mask)
+
+    def postprocess_fn(self, seg_mask: Tensor, cont_mask: Tensor = None) -> Tensor:
+        return NucleiInstances.from_mask(seg_mask, cont_mask).as_tensor()
+
+
+class DistPostProcess(Postprocess):
+    """
+    Postprocessing based on the distance map.
+
+    The postprocessing strategy by Naylor et al. 2019 is used to identify nuclei instances.
+    """
+    def __init__(self, param: int, thresh: Union[int, float]):
+        self.param = param
+        self.thresh = thresh
+
+    def __call__(self, dist: Tensor) -> Union[Tensor, Tuple[Tensor, ...]]:
+        return self.postprocess(dist)
+
+    def postprocess_fn(self, dist_map: Tensor, *args) -> Tensor:
+        return NucleiInstances.from_dist_map(dist_map, self.param, self.thresh).as_tensor()
+
+
+class HVPostProcess(Postprocess):
+    """
+   Postprocessing based on the horizontal and vertical distance map.
+
+   The postprocessing strategy by Graham et al. 2019 is used to identify nuclei instances.
+   """
+    def __init__(self):
+        pass
+
+    def __call__(self, hv_map: Tensor, seg: Tensor) -> Union[Tensor, Tuple[Tensor, ...]]:
+        seg_mask = seg >= 0.5
+        return self.postprocess(hv_map, seg_mask)
+
+    def postprocess_fn(self, hv_map: Tensor, seg_mask: Tensor) -> Tensor:
+        return NucleiInstances.from_hv_map(hv_map, seg_mask).as_tensor()
+
+
+if __name__ == '__main__':
+    from data.MoNuSeg.data_module import MoNuSegDataModule
+    from data.MoNuSeg.ground_truth import NucleiInstances
+    from data.MoNuSeg.illustrator import Picture
+
+    data_module = MoNuSegDataModule(
+        seg_masks=True,
+        cont_masks=True,
+        dist_maps=True,
+        hv_maps=False,
+        labels=False,
+        data_root="../datasets"
+    )
+    # data_module.prepare_data()
+    data_module.setup(stage="test")
+    test_loader = data_module.test_dataloader()
+    img, seg_mask, cont_mask, dist_map, inst_gt = next(iter(test_loader))
+
+    index = 7
+    inst_pred = DistPostProcess(param=2, thresh=0)(dist_map)
+
+    # pd, gt = inst_gt[index], inst_gt[index]
+    # pd_labeled, gt_labeled = NucleiInstances.from_inst(pd).to_labeled_inst(), NucleiInstances.from_inst(gt).to_labeled_inst()
+    # Picture(pd_labeled).show(), Picture(gt_labeled).show()
+
+    print(inst_gt[index].shape, inst_pred[index].shape)
+    # Picture.from_tensor(img[index]).show()
+    Picture.from_tensor(img[index], inst_gt[index]).show()
+    Picture.from_tensor(img[index], inst_pred[index]).show()
+
---
+++ postprocessing/postprocesses_base.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Tuple, Union
+from torch import Tensor
+
+from evaluation.utils import is_batched
+
+
+class Postprocess(ABC):
+    """
+    Base class for postprocessing.
+    """
+
+    @abstractmethod
+    def __call__(self, *args) -> Union[Tensor, Tuple[Tensor, ...]]:
+        """
+        Define here the postprocessing pipeline.
+
+        First, you might want to perform some operation on batch level.
+        Finally, you will probably want to call the postprocess function.
+        """
+
+    @abstractmethod
+    def postprocess_fn(self, *args) -> Tensor:
+        """
+        Define here the postprocessing function.
+        """
+        pass
+
+    def postprocess(self, tensor_0: Tensor, tensor_1: Optional[Tensor] = None) -> Union[Tensor, Tuple[Tensor, ...]]:
+        """
+        Iterates over the batched tensor(s) and applies the postprocess_fn function to them.
+        """
+        if not is_batched(tensor_0):
+            raise ValueError(f"Tensor_0 should have shape (B, C, W, H). Got instead {tensor_0.shape}.")
+        if tensor_1 is not None and not is_batched(tensor_1):
+            raise ValueError(f"Tensor_1 should have shape (B, C, W, H). Got instead {tensor_1.shape}.")
+        batch_size = tensor_0.shape[0]
+        device = tensor_0.device
+
+        if batch_size == 1:
+            if tensor_1 is None:
+                return self.postprocess_fn(tensor_0).to(device)
+            else:
+                return self.postprocess_fn(tensor_0, tensor_1).to(device)
+        else:
+            if tensor_1 is None:
+                return tuple(self.postprocess_fn(t).to(device) for t in tensor_0)
+            else:
+                return tuple(self.postprocess_fn(t0, t1).to(device) for t0, t1 in zip(tensor_0, tensor_1))
