c16f651dd27a5558509da0c25e966cf95df702ad
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-05-22 11:34:29 +0000

    Script for dataset preparation

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,16 +1,49 @@
-from data.MoNuSeg.data_module import MoNuSegDataModule
+from typing import Union, Tuple
+
+from data.MoNuSeg.dataset import MoNuSeg
+from data.MoNuSeg.dataset_creator import MoNuSegCreator
+from data.MoNuSeg.dataset_patcher import MoNuSegPatcher
+from transformation.transformations import ToTensor
+
+
+def prepare_data(data_split: str = "Train Kaggle", data_root: str = "datasets",
+                 seg_masks: bool = True, cont_masks: bool = True, dist_maps: bool = True, hv_maps: bool = True,
+                 img_size: Union[Tuple[int, int], int] = (256, 256)
+                 ):
+    # Create ground truths
+    creator = MoNuSegCreator(root=data_root)
+    creator.save_ground_truths(
+        segmentation_masks=seg_masks,
+        contour_masks=cont_masks,
+        distance_maps=dist_maps,
+        hv_distance_maps=hv_maps
+    )
+    # Cut
+    patcher = MoNuSegPatcher(
+        dataset=MoNuSeg(
+            root=data_root,
+            segmentation_masks=seg_masks,
+            contour_masks=cont_masks,
+            distance_maps=dist_maps,
+            hv_distance_maps=hv_maps,
+            instances=True,
+            transforms=ToTensor(),
+            dataset=data_split
+        )
+    )
+    patcher.split_and_save(patch_size=img_size)
+
 
 def main():
-    data_module = MoNuSegDataModule(
+    prepare_data(
         seg_masks=True,
         cont_masks=True,
         dist_maps=True,
         hv_maps=True,
-        labels=True,
         img_size=(256, 256),
         data_root="datasets"
     )
-    data_module.prepare_data()
+
 
 if __name__ == "__main__":
     main()
