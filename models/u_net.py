2ef87028cb71f9dde764aa8364e3ae52997ec376
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-07-06 11:04:28 +0000

    Added new Networks

--- models/u_net.py
+++ models/u_net.py
@@ -1,5 +1,8 @@
+from typing import Dict, Tuple
+
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 class ConvBlock(nn.Module):
@@ -23,15 +26,26 @@ class ConvBlock(nn.Module):
         return self.relu(x)
 
 
-class UpConv(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+class Up(nn.Module):
+    def __init__(self, in_channels: int, mode: str = "bilinear"):
         super().__init__()
-        self.up = nn.UpsamplingBilinear2d(scale_factor=(2., 2.))
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        if mode == "bilinear":
+            self.up = nn.UpsamplingBilinear2d(scale_factor=(2., 2.))
+        elif mode == "transposed":
+            self.up = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=2,
+                                         stride=2)
+        elif mode == "up-convolution":
+            # The original U-Net (Ronneberger el al. 2015) used upsampling followed by a convolution.
+            self.up = nn.Sequential(
+                nn.UpsamplingBilinear2d(scale_factor=(2., 2.)),
+                # Here 3x3 conv instead of 2x2 conv as in the original U-Net
+                nn.Conv2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=3, stride=1, padding=1)
+            )
+        else:
+            raise ValueError(f"Mode should be bilinear, transposed or up-convolution. Got instead {mode}.")
 
     def forward(self, x):
-        x = self.up(x)
-        return self.conv(x)
+        return self.up(x)
 
 
 class EncodBlock(nn.Module):
@@ -46,9 +60,9 @@ class EncodBlock(nn.Module):
 
 
 class DecodBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, up_mode: str = "bilinear"):
         super().__init__()
-        self.up_conv = UpConv(in_channels=in_channels, out_channels=out_channels)
+        self.up_conv = Up(in_channels=in_channels, mode=up_mode)
         self.conv_block = ConvBlock(in_channels=in_channels, out_channels=out_channels)
 
     def forward(self, x, skip):
@@ -57,23 +71,30 @@ class DecodBlock(nn.Module):
         return self.conv_block(x)
 
 
-class UNet(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, feature_num: int):
+class OrigUNet(nn.Module):
+    """
+    Original U-Net as described by Ronneberger et al. 2015.
+
+    Modifications:
+    * BatchNorm instead of Dropout
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, base_channels: int = 64):
         super().__init__()
         # Encoder
-        self.e1 = EncodBlock(in_channels=in_channels, out_channels=feature_num)
-        self.e2 = EncodBlock(in_channels=feature_num, out_channels=2 * feature_num)
-        self.e3 = EncodBlock(in_channels=2 * feature_num, out_channels=4 * feature_num)
-        self.e4 = EncodBlock(in_channels=4 * feature_num, out_channels=8 * feature_num)
+        self.e1 = EncodBlock(in_channels=in_channels, out_channels=base_channels)
+        self.e2 = EncodBlock(in_channels=base_channels, out_channels=2 * base_channels)
+        self.e3 = EncodBlock(in_channels=2 * base_channels, out_channels=4 * base_channels)
+        self.e4 = EncodBlock(in_channels=4 * base_channels, out_channels=8 * base_channels)
         # Bottleneck
-        self.b = ConvBlock(in_channels=8 * feature_num, out_channels=16 * feature_num)
+        self.b = ConvBlock(in_channels=8 * base_channels, out_channels=16 * base_channels)
         # Decoder
-        self.d1 = DecodBlock(in_channels=16 * feature_num, out_channels=8 * feature_num)
-        self.d2 = DecodBlock(in_channels=8 * feature_num, out_channels=4 * feature_num)
-        self.d3 = DecodBlock(in_channels=4 * feature_num, out_channels=2 * feature_num)
-        self.d4 = DecodBlock(in_channels=2 * feature_num, out_channels=feature_num)
+        self.d1 = DecodBlock(in_channels=16 * base_channels, out_channels=8 * base_channels, up_mode="up-convolution")
+        self.d2 = DecodBlock(in_channels=8 * base_channels, out_channels=4 * base_channels, up_mode="up-convolution")
+        self.d3 = DecodBlock(in_channels=4 * base_channels, out_channels=2 * base_channels, up_mode="up-convolution")
+        self.d4 = DecodBlock(in_channels=2 * base_channels, out_channels=base_channels, up_mode="up-convolution")
         # Classifier
-        self.c = nn.Conv2d(in_channels=feature_num, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.c = nn.Conv2d(in_channels=base_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         # Encoder
@@ -90,3 +111,133 @@ class UNet(nn.Module):
         x = self.d4(x, s1)
         # Classifier
         return self.c(x)
+
+
+class UNet(nn.Module):
+    """
+    U-Net with three down/up-sampling blocks.
+
+    Additional layer after the final U-Net layer:
+    * Connector layer: 3x3 Conv, BN, ReLU
+    * Output layer(s): 1x1 Conv
+    """
+
+    def __init__(self, in_channels: int = 3, base_channels: int = 32, con_channels: int = 128, mode: str = "seg"):
+        super().__init__()
+        # Encoder
+        self.e0 = EncodBlock(in_channels=in_channels, out_channels=base_channels)
+        self.e1 = EncodBlock(in_channels=base_channels, out_channels=2 * base_channels)
+        self.e2 = EncodBlock(in_channels=2 * base_channels, out_channels=4 * base_channels)
+        # Bottleneck
+        self.b = ConvBlock(in_channels=4 * base_channels, out_channels=4 * base_channels)
+        # Decoder
+        self.d0 = DecodBlock(in_channels=8 * base_channels, out_channels=2 * base_channels)
+        self.d1 = DecodBlock(in_channels=4 * base_channels, out_channels=base_channels)
+        self.d2 = DecodBlock(in_channels=2 * base_channels, out_channels=base_channels)
+        # Connector
+        self.c = nn.Sequential(
+            nn.Conv2d(in_channels=base_channels, out_channels=con_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=con_channels),
+            nn.ReLU()
+        )
+        # Output
+        if mode == "seg":
+            self.o0 = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+            self.o1 = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+        elif mode == "dist":
+            self.o0 = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+        elif mode == "hv":
+            self.o0 = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+            self.o1 = nn.Conv2d(in_channels=con_channels, out_channels=2, kernel_size=1, stride=1, padding=0)
+        else:
+            raise ValueError(f"Mode should be seg, dist or hv. Got instead {mode}.")
+        self.mode = mode
+
+    def forward(self, x) -> Dict[str, Tensor]:
+        # Encoder
+        x, s0 = self.e0(x)
+        x, s1 = self.e1(x)
+        x, s2 = self.e2(x)
+        # Bottleneck
+        x = self.b(x)
+        # Decoder
+        x = self.d0(x, s2)
+        x = self.d1(x, s1)
+        x = self.d2(x, s0)
+        # Connector
+        x = self.c(x)
+        # Output
+        if self.mode == "seg":
+            return {"seg_mask": self.o0(x), "cont_mask": self.o1(x)}
+        elif self.mode == "dist":
+            return {"dist_map": self.o0(x)}
+        elif self.mode == "hv":
+            return {"seg_mask": self.o0(x), "hv_map": self.o1(x)}
+
+
+class UNetDualDecoder(nn.Module):
+    """
+    U-Net with one encoder and two decoder.
+
+    The encoder and the decoder consist of three down-sampling and up-sampling blocks, respectively.
+    """
+
+    def __init__(self, in_channels: int = 3, base_channels: int = 32, con_channels: int = 128, mode: str = "seg"):
+        super().__init__()
+        # Encoder
+        self.e0 = EncodBlock(in_channels=in_channels, out_channels=base_channels)
+        self.e1 = EncodBlock(in_channels=base_channels, out_channels=2 * base_channels)
+        self.e2 = EncodBlock(in_channels=2 * base_channels, out_channels=4 * base_channels)
+        # Bottleneck
+        self.b = ConvBlock(in_channels=4 * base_channels, out_channels=4 * base_channels)
+        # Decoder a
+        self.da0 = DecodBlock(in_channels=8 * base_channels, out_channels=2 * base_channels)
+        self.da1 = DecodBlock(in_channels=4 * base_channels, out_channels=base_channels)
+        self.da2 = DecodBlock(in_channels=2 * base_channels, out_channels=base_channels)
+        # Decoder b
+        self.db0 = DecodBlock(in_channels=8 * base_channels, out_channels=2 * base_channels)
+        self.db1 = DecodBlock(in_channels=4 * base_channels, out_channels=base_channels)
+        self.db2 = DecodBlock(in_channels=2 * base_channels, out_channels=base_channels)
+        # Connector a
+        self.ca = nn.Sequential(
+            nn.Conv2d(in_channels=base_channels, out_channels=con_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=con_channels),
+            nn.ReLU()
+        )
+        # Connector b
+        self.cb = nn.Sequential(
+            nn.Conv2d(in_channels=base_channels, out_channels=con_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=con_channels),
+            nn.ReLU()
+        )
+        # Output
+        self.oa = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+        if mode == "seg":
+            self.ob = nn.Conv2d(in_channels=con_channels, out_channels=1, kernel_size=1, stride=1, padding=0)
+        elif mode == "hv":
+            self.ob = nn.Conv2d(in_channels=con_channels, out_channels=2, kernel_size=1, stride=1, padding=0)
+        else:
+            raise ValueError(f"Mode should be seg or hv. Got instead {mode}.")
+        self.mode = mode
+
+    def forward(self, x) -> Tuple[Tensor, Tensor]:
+        # Encoder
+        x, s0 = self.e0(x)
+        x, s1 = self.e1(x)
+        x, s2 = self.e2(x)
+        # Bottleneck
+        x = self.b(x)
+        # Decoder a
+        xa = self.da0(x, s2)
+        xa = self.da1(xa, s1)
+        xa = self.da2(xa, s0)
+        # Decoder b
+        xb = self.db0(x, s2)
+        xb = self.db1(xb, s1)
+        xb = self.db2(xb, s0)
+        # Connector a
+        xa = self.ca(xa)
+        # Connector b
+        xb = self.cb(xb)
+        # Output
+        return self.oa(xa), self.ob(xb)
