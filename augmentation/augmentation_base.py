023150370d0a3df51def401b7ac6eeb993972acc
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-07-06 10:38:25 +0000

    Adoption to changed MoNuSeg dataloader; Base class for augmentations to be applied only to the image (e.g., GaussianBlur) and not to the gt

--- augmentation/augmentation_base.py
+++ augmentation/augmentation_base.py
@@ -1,16 +1,42 @@
-from abc import ABC, abstractmethod
-from typing import List
 import random
-import torch
+from abc import ABC, abstractmethod
+from typing import Dict
+
+from torch import Tensor
 
 
 class Augmentation(ABC):
     """Base class for image augmentations"""
 
-    def __call__(self, imgs: List[torch.Tensor]) -> List[torch.Tensor]:
+    @abstractmethod
+    def __call__(self, gt: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        pass
+
+
+class AugmentAll(Augmentation):
+    """Base class for image augmentations to be applied to all ground truths."""
+
+    def __call__(self, gt: Dict[str, Tensor]) -> Dict[str, Tensor]:
         seed = random.random()
-        return [self.transform(img, seed) for img in imgs]
+        for key, value in gt.items():
+            gt[key] = self.transform(value, seed)
+        return gt
 
     @abstractmethod
-    def transform(self, img: torch.Tensor, seed: float) -> torch.Tensor:
+    def transform(self, img: Tensor, seed: float) -> Tensor:
+        pass
+
+
+class AugmentOne(Augmentation):
+    """Base class for image augmentations to be applied to only one ground truth."""
+
+    def __init__(self, name: str = "img"):
+        self.name = name
+
+    def __call__(self, gt: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        gt[self.name] = self.transform(gt[self.name])
+        return gt
+
+    @abstractmethod
+    def transform(self, img: Tensor) -> Tensor:
         pass
