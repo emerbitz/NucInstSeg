fc1c61250cf8b6d970f4096935bf6047bfdf0109
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-07-06 10:30:43 +0000

    Correct serval bugs (e.g., in as_ndarray, to_hv_map); from_seg_mask still needs revision; reduced boilerplate code

--- data/MoNuSeg/ground_truth.py
+++ data/MoNuSeg/ground_truth.py
@@ -1,25 +1,29 @@
 from pathlib import Path
-from typing import List, Union, Tuple
+from typing import List, Optional, Union
 from xml.dom import minidom
+
 import numpy as np
 import torch
-from torch import Tensor
 from scipy.ndimage import center_of_mass, distance_transform_cdt
 from skimage.draw import polygon2mask
+from skimage.morphology import binary_dilation, square, remove_small_objects
 from skimage.segmentation import find_boundaries, flood
-from skimage.morphology import dilation, square, remove_small_objects
-from postprocessing.naylor_postprocessing import naylor_postprocess
-from postprocessing.graham_postprocessing import graham_postprocess
+from torch import Tensor
 
-from data.MoNuSeg.utils import get_bbox
+from data.MoNuSeg.utils import get_bbox, prob_to_mask, cuda_tensor_to_ndarray
+from postprocessing.graham_postprocessing import graham_postprocess
+from postprocessing.naylor_postprocessing import naylor_postprocess
 
 
 class NucleiInstances:
+    SHAPE_KUMAR = (1000, 1000)
 
     def __init__(self, nuclei_instances: List[np.ndarray]):
+        if nuclei_instances:
+            self.shape = nuclei_instances[0].shape
+        else:
+            self.shape = None
         self.nuc_inst = nuclei_instances
-        # Premise: All nuclei instances have the same shape
-        self.shape = (1000, 1000)  # Hard coded; adaption needed for other dataset
 
     def __len__(self) -> int:
         return len(self.nuc_inst)
@@ -30,7 +34,10 @@ class NucleiInstances:
     def as_ndarray(self) -> np.ndarray:
         """Converts the nuclei instances as List[np.ndarray] into a np.ndarray of shape (H, W, #nuclei)."""
         array = np.array(self.nuc_inst)
-        return np.transpose(array, (1, 2, 0))
+        if self.nuc_inst:
+            return np.transpose(array, (1, 2, 0))
+        else:  # No nuclei instances
+            return array
 
     def as_tensor(self) -> Tensor:
         """Converts the nuclei instances as List[np.ndarray] into a tensor of shape (#nuclei, H, W)."""
@@ -43,35 +50,43 @@ class NucleiInstances:
         Extracts the nuclei instances from a xml-file.
         """
 
-        img_shape = (1000, 1000)
         dom = minidom.parse(str(label))
         tree = dom.documentElement
         regions = tree.getElementsByTagName("Region")
         nuc_inst = []
+        empty_regions = 0
         for region in regions:
             vertexes = region.getElementsByTagName("Vertex")
             polygon = []
             for vertex in vertexes:
                 polygon.append((float(vertex.getAttribute("Y")), float(vertex.getAttribute("X"))))
-            mask = polygon2mask(image_shape=img_shape, polygon=polygon)
+            mask = polygon2mask(image_shape=NucleiInstances.SHAPE_KUMAR, polygon=polygon)
             if mask.any():  # Check for emtpy masks
                 nuc_inst.append(mask)
+            else:
+                empty_regions += 1
+        print(f"Found {empty_regions} empty regions in {str(label)}")
         return NucleiInstances(nuc_inst)
 
     @staticmethod
-    def from_mask(seg_mask: Tensor, cont_mask: Tensor = None) -> "NucleiInstances":
-        """Extracts nuclei instances from a segmentation mask via flooding."""
+    def from_seg(seg: Tensor, cont: Optional[Tensor] = None, seg_thresh: Union[str, float] = 0.5,
+                 cont_thresh: Union[str, float] = 0.5) -> "NucleiInstances":
+        """
+        Extracts nuclei instances from a segmentation via flooding.
 
-        seg_mask = seg_mask.cpu()
-        seg_mask = seg_mask.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
-        remove_small_objects(seg_mask, min_size=15, out=seg_mask)  # Noise suppression; hard coded
-        if cont_mask is not None:
-            cont_mask = cont_mask.cpu()
-            cont_mask = cont_mask.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
-            mask = seg_mask * np.logical_not(
-                cont_mask)  # faster than np.logical_and(seg_mask, np.logical_not(cont_mask))
+        If a contour is provided, then the contour is subtracted from the segmentation prior to flooding.
+        """
+        seg = cuda_tensor_to_ndarray(seg)
+        seg_mask = prob_to_mask(seg, thresh=seg_thresh)
+        remove_small_objects(seg_mask, min_size=15, out=seg_mask)  # Noise suppression; hard coded # Maybe insert later
+        if cont is not None:
+            cont = cuda_tensor_to_ndarray(cont)
+            cont_mask = prob_to_mask(cont, cont_thresh)
+            mask = seg_mask * np.logical_not(cont_mask)
         else:
             mask = seg_mask
+        # Noise suppression:
+        # remove_small_objects(mask, min_size=30, out=mask)
 
         height, width = mask.shape
         nuclei = []
@@ -80,9 +95,19 @@ class NucleiInstances:
                 if mask[y, x]:
                     nucleus = flood(mask, seed_point=(y, x))  # Region growing with 8-connected neighborhood
                     mask = np.logical_xor(mask, nucleus)  # Removes the nucleus from the mask
-                    if cont_mask is not None:
-                        nucleus = dilation(nucleus, square(3))  # * seg_mask
+                    # if cont is not None:
+                    #     nucleus = binary_dilation(nucleus, square(3))  # * seg_mask
                     nuclei.append(nucleus)
+        if cont is not None:
+            flag = True
+            while flag:
+                flag = False
+                for idx, nucleus in enumerate(nuclei):
+                    a = binary_dilation(nucleus, footprint=square(3, dtype=bool), out=None) * cont_mask
+                    if a.any():
+                        nuclei[idx] += a
+                        cont_mask = cont_mask * ~a
+                        flag = True
         return NucleiInstances(nuclei)
 
     @staticmethod
@@ -94,24 +119,22 @@ class NucleiInstances:
         dist_map[dist_map > 255] = 255
         dist_map = dist_map.byte()
 
-        dist_map = dist_map.cpu()
-        dist_map = dist_map.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
+        dist_map = cuda_tensor_to_ndarray(dist_map)
 
         labeled_inst = naylor_postprocess(dist_map, param, thresh)
         return NucleiInstances.from_labeled_inst(labeled_inst)
 
     @staticmethod
-    def from_hv_map(hv_map: Tensor, seg_mask: Tensor) -> "NucleiInstances":
+    def from_hv_map(hv_map: Tensor, seg: Tensor, seg_thresh: Union[str, float] = 0.5) -> "NucleiInstances":
         """
         Extracts nuclei instances from a horizontal and vertical distance map via the postprocessing strategy by
          Graham et al. 2019.
         """
 
-        hv_map = hv_map.cpu()
-        hv_map = hv_map.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
-        seg_mask = seg_mask.cpu()
-        seg_mask = seg_mask.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
-        seg_mask = seg_mask.astype(bool)
+        hv_map = cuda_tensor_to_ndarray(hv_map)
+
+        seg = cuda_tensor_to_ndarray(seg)
+        seg_mask = prob_to_mask(seg, seg_thresh)
 
         labeled_inst = graham_postprocess(hv_map, seg_mask)
 
@@ -136,7 +159,7 @@ class NucleiInstances:
         Extracts nuclei instances as list of arrays from a stacked tensor.
         """
         inst = inst.cpu()
-        inst = inst.squeeze().numpy()  # Conversion to np.ndarray of shape (H, W)
+        inst = inst.numpy()  # Conversion to np.ndarray of shape (C, H, W)
         nuclei = [nucleus for nucleus in inst]
         return NucleiInstances(nuclei)
 
@@ -194,7 +217,7 @@ class NucleiInstances:
             map[inst] = dist[inst]
         return map
 
-    def to_hv_map(self) -> np.ndarray:
+    def to_hv_map(self, order: str = "CHW") -> np.ndarray:
         """
         Generates a horizontal and a vertical distance map form the nuclei instances.
         """
@@ -213,23 +236,25 @@ class NucleiInstances:
             # cntr = (crop.shape[0] // 2, crop.shape[1] // 2)
 
             # Calculate centered row and column values:
-            row = np.arange(crop.shape[1], dtype=float) - cntr[1]
-            col = np.arange(crop.shape[0], dtype=float) - cntr[0]
+            row = np.arange(crop.shape[1], dtype=np.float32) - cntr[1]
+            col = np.arange(crop.shape[0], dtype=np.float32) - cntr[0]
             # Normalize to [-1, 1]:
             row[row < 0] /= -row[0]
             row[row > 0] /= row[-1]
             col[col < 0] /= -col[0]
             col[col > 0] /= col[-1]
 
-            # Determine horizontal and vertical distance map for the crop:
-            h_dist_map = np.where(crop, row, crop)
-            v_dist_map = np.where(crop.T, col, crop.T).T
+            # Insert horizontal and vertical distance map:
+            h_map[y_min:y_max, x_min:x_max] = np.where(crop, row, h_map[y_min:y_max, x_min:x_max])
+            v_map[y_min:y_max, x_min:x_max] = np.where(crop.T, col, v_map[y_min:y_max, x_min:x_max].T).T
 
-            # Paste crop in horizontal and vertical distance map:
-            h_map[y_min:y_max, x_min:x_max] = h_dist_map
-            v_map[y_min:y_max, x_min:x_max] = v_dist_map
         # Return h_map, v_map
-        return np.dstack((h_map, v_map))  # Shape: (H, W, C)
+        if order == "CHW":
+            return np.stack((h_map, v_map))  # Shape (C, H, W)
+        elif order == "HWC":
+            return np.dstack((h_map, v_map))  # Shape (H, W, C)
+        else:
+            raise ValueError(f"Order should be CHW or HWC. Got instead {order}")
 
 
 def setup():
@@ -251,7 +276,6 @@ def setup():
 
 if __name__ == "__main__":
     from data.MoNuSeg.illustrator import Picture
-    from timeit import timeit
 
     # res = timeit(setup="from data.MoNuSeg.ground_truth import NucleiInstances, setup; inst=setup()[0]",
     #              stmt="NucleiInstances([inst]).to_hv_map()", number=int(1e5))
