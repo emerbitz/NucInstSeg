1209ad49c6e4db93740e2674cc122d19d470db6c
emerbitz <78315455+emerbitz@users.noreply.github.com>
2023-11-04 15:42:06 +0000

    Added option to freeze ResNet backbone

--- models/al_net.py
+++ models/al_net.py
@@ -11,10 +11,12 @@ from models.building_blocks import ALModule, CELayer, VolUp, Up, BLUpConvBNReLU
 class ALEncoder(nn.Module):
     """Consists of a pretrained ResNet34 with a CE layer before every pooling (except for the first pooling XD)"""
 
-    def __init__(self, in_channels: int):
+    def __init__(self, in_channels: int, freeze_resnet: bool = False):
         super().__init__()
         self.resnet = models.resnet34(weights='IMAGENET1K_V1')
-        # self.resnet.requires_grad_(False)
+        if freeze_resnet:
+            for param in self.resnet.parameters():
+                param.requires_grad = False
         self.resnet.conv1 = nn.Conv2d(in_channels, 32, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.resnet.bn1 = nn.BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
         self.resnet.layer1[0].conv1 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
@@ -103,43 +105,56 @@ class ALDecoderAuxiliary(nn.Module):
 class ALNet(nn.Module):
     """Attention Learning Network (AL-Net) as introduced by Zhao et al. 2022"""
 
-    def __init__(self, in_channels: int = 3, mode: str = "noname", net_params: Optional[dict] = None):
+    def __init__(self, in_channels: int = 3, mode: str = "contour", net_params: Optional[dict] = None):
         super().__init__()
         # Define default hyperparameters:
         default_params = {
             "auxiliary_task": True,  # Dis-/enables optional auxiliary task for modes 'baseline', 'yang' and 'naylor'
+            "naylor_aux_task": "cont_mask",  # Auxiliary task for Naylor postprocessing
+            "freeze_resnet": False,
+            "bridge": True
         }
         if net_params is None:
             net_params = {}
         # Overwrite default hyperparameters:
         net_params = {**default_params, **net_params}
 
-        self.e = ALEncoder(in_channels=in_channels)
-        self.b = ALBridge(base_channels=32)
+        self.e = ALEncoder(in_channels=in_channels, freeze_resnet=net_params["freeze_resnet"])
+        if net_params["bridge"]:
+            self.b = ALBridge(base_channels=32)
+        else:
+            self.b = None
         self.dm = ALDecoderMain(base_channels=32, out_channels=1)
         if mode in ["graham", "exprmtl"]:
             self.da = ALDecoderAuxiliary(out_channels=2)
         elif not net_params["auxiliary_task"]:
             self.da = None
+        elif mode == "naylor" and net_params["auxiliary_task"] and net_params["naylor_aux_task"] == "hv_map":
+            self.da = ALDecoderAuxiliary(out_channels=2)
         else:
             self.da = ALDecoderAuxiliary(out_channels=1)
 
         self.mode = mode  # Fetched by NetModel: Selects loss term and postprocessing scheme
         self.auxiliary_task = net_params["auxiliary_task"]  # Fetched by NetModel: Adapts loss term
+        self.naylor_aux_task = net_params["naylor_aux_task"]  # Fetched by NetModel: Adapts loss term
         if not self.auxiliary_task and mode not in ["baseline", "yang", "naylor"]:
             raise ValueError(f"No auxiliary task is only support for modes 'baseline', 'yang', 'naylor'. "
                              f"Got instead {self.mode}.")
 
     def forward(self, x: Tensor) -> Dict[str, Tensor]:
         x, *skip = self.e(x)
-        skip = self.b(skip)
+        if self.b is not None:
+            skip = self.b(skip)
         x_main = self.dm(x, skip)
         if self.auxiliary_task:
             x_aux = self.da(skip)
-            if self.mode in ["baseline", "noname", "yang"]:
+            if self.mode in ["baseline", "contour", "yang"]:
                 return {"seg_mask": x_main, "cont_mask": x_aux}
             elif self.mode == "naylor":
-                return {"dist_map": x_main, "cont_mask": x_aux}
+                if self.naylor_aux_task == "cont_mask":
+                    return {"dist_map": x_main, "cont_mask": x_aux}
+                else:
+                    return {"dist_map": x_main, "hv_map": x_aux}
             elif self.mode in ["graham", "exprmtl"]:
                 return {"seg_mask": x_main, "hv_map": x_aux}
         else:
@@ -147,98 +162,3 @@ class ALNet(nn.Module):
                 return {"seg_mask": x_main}
             elif self.mode == "naylor":
                 return {"dist_map": x_main}
-
-
-class ALNetDualDecoder(nn.Module):
-    """
-    Attention Learning Network (AL-Net) with two decoder.
-    """
-
-    def __init__(self, in_channels: int = 3, mode: str = "noname", net_params: Optional[dict] = None):
-        super().__init__()
-        # Define default hyperparameters:
-        default_params = {
-            "auxiliary_task": True,  # Dis-/enables optional auxiliary task for modes 'baseline', 'yang' and 'naylor'
-        }
-        if net_params is None:
-            net_params = {}
-        # Overwrite default hyperparameters:
-        net_params = {**default_params, **net_params}
-
-        self.e = ALEncoder(in_channels=in_channels)
-        self.bm = ALBridge(base_channels=32)
-        self.ba = ALBridge(base_channels=32)
-        self.dm = ALDecoderMain(base_channels=32, out_channels=1)
-        if mode in ["graham", "exprmtl"]:
-            self.da = ALDecoderMain(base_channels=32, out_channels=2)
-        elif not net_params["auxiliary_task"]:
-            self.da = None
-        else:
-            self.da = ALDecoderMain(base_channels=32, out_channels=1)
-
-        self.mode = mode  # Fetched by NetModel: Selects loss term and postprocessing scheme
-        self.auxiliary_task = net_params["auxiliary_task"]  # Fetched by NetModel: Adapts loss term
-        if not self.auxiliary_task and mode not in ["baseline", "yang", "naylor"]:
-            raise ValueError(f"No auxiliary task is only support for modes 'baseline', 'yang', 'naylor'. "
-                             f"Got instead {self.mode}.")
-
-    def forward(self, x: Tensor) -> Dict[str, Tensor]:
-        x, *skip = self.e(x)
-        skip_main = self.bm(skip)
-        skip_aux = self.ba(skip)
-        x_main = self.dm(x, skip_main)
-        if self.auxiliary_task:
-            x_aux = self.da(x, skip_aux)
-            if self.mode in ["baseline", "noname", "yang"]:
-                return {"seg_mask": x_main, "cont_mask": x_aux}
-            elif self.mode == "naylor":
-                return {"dist_map": x_main, "cont_mask": x_aux}
-            elif self.mode in ["graham", "exprmtl"]:
-                return {"seg_mask": x_main, "hv_map": x_aux}
-        else:
-            if self.mode in ["baseline", "yang"]:
-                return {"seg_mask": x_main}
-            elif self.mode == "naylor":
-                return {"dist_map": x_main}
-
-
-class ALNetDualDecoderHV(nn.Module):
-    """
-    Attention Learning Network (AL-Net) with two decoder branches.
-    """
-
-    def __init__(self, in_channels: int = 3, mode: str = "graham", net_params: Optional[dict] = None):
-        super().__init__()
-        # Define default hyperparameters:
-        default_params = {
-            "auxiliary_task": True,  # Dis-/enables optional auxiliary task for modes 'baseline', 'yang' and 'naylor'
-        }
-        if net_params is None:
-            net_params = {}
-        # Overwrite default hyperparameters:
-        net_params = {**default_params, **net_params}
-
-        self.e = ALEncoder(in_channels=in_channels)
-        self.b0 = ALBridge(base_channels=32)
-        self.b1 = ALBridge(base_channels=32)
-        self.dm0 = ALDecoderMain(base_channels=32, out_channels=1)
-        self.dm1 = ALDecoderMain(base_channels=32, out_channels=2)
-        self.da0 = ALDecoderAuxiliary(out_channels=1)
-        self.da1 = ALDecoderAuxiliary(out_channels=1)
-
-        if mode not in ["graham", "exprmtl"]:
-            raise ValueError(f"Mode should be 'graham' or 'exprmtl'. Got instead {mode}")
-
-        self.mode = mode  # Fetched by NetModel: Selects loss term and postprocessing scheme
-        self.auxiliary_task = net_params["auxiliary_task"]  # Fetched by NetModel: Adapts loss term
-
-    def forward(self, x: Tensor) -> Dict[str, Tensor]:
-        x, *skip = self.e(x)
-        skip_0 = self.b0(skip)
-        skip_1 = self.b1(skip)
-        x_main_0 = self.dm0(x, skip_0)
-        x_main_1 = self.dm1(x, skip_1)
-        x_aux_0 = self.da0(skip_0)
-        x_aux_1 = self.da1(skip_1)
-
-        return {"seg_mask": x_main_0, "cont_mask": x_aux_0, "hv_map": x_main_1, "dist_map": x_aux_1}
